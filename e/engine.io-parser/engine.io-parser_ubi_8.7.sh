ebf4942c3cafc46c15450e0b92cc8cfb2bea13d6
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2023-05-29 15:57:35 +0530

    Update engine.io-parser_ubi_8.7.sh

--- e/engine.io-parser/engine.io-parser_ubi_8.7.sh
+++ e/engine.io-parser/engine.io-parser_ubi_8.7.sh
@@ -26,13 +26,12 @@ HOME_DIR=${PWD}
 sudo yum install git wget sudo curl tar -y
 
 cd $HOME_DIR
-wget https://nodejs.org/dist/v14.21.2/node-v14.21.2-linux-ppc64le.tar.gz
-tar -xzf node-v14.21.2-linux-ppc64le.tar.gz
-export PATH=$HOME_DIR/node-v14.21.2-linux-ppc64le/bin:$PATH
-node -v
-npm -v
-which npm
-npm install -g yarn
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 14
+nvm use 14
 
 #1. Building ngrok version 3.4.0 from github
 git clone https://github.com/bubenshchykov/ngrok.git && cd ngrok
