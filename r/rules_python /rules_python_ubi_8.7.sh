84b7dfe284fd3e6501e595fe89da1ef41187f1fb
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2023-12-06 11:04:09 +0530

    Update rules_python_ubi_8.7.sh

--- r/rules_python /rules_python_ubi_8.7.sh
+++ r/rules_python /rules_python_ubi_8.7.sh
@@ -2,7 +2,7 @@
 # -----------------------------------------------------------------------------
 #
 # Package          : rules_python
-# Version          : 0.27.0
+# Version          : 0.27.1
 # Source repo      : https://github.com/bazelbuild/rules_python
 # Tested on        : UBI 8.7
 # Language         : Starlark,Python
@@ -10,7 +10,7 @@
 # Script License   : Apache License, Version 2 or later
 # Maintainer       : Vinod K <Vinod.K1@ibm.com>
 #
-# Disclaimer       : This script has been tested in root mode on given
+# Disclaimer       : This script has been tested in non-root mode on given
 # ==========         platform using the mentioned version of the package.
 #                    It may not work as expected with newer versions of the
 #                    package and/or distribution. In such case, please
@@ -19,20 +19,21 @@
 # ----------------------------------------------------------------------------
 
 PACKAGE_NAME="rules_python"
-PACKAGE_VERSION=${1:-"0.27.0"}
+PACKAGE_VERSION=${1:-"0.27.1"}
 PACKAGE_URL=https://github.com/bazelbuild/rules_python
 
 # Install dependencies
-yum install -y git make wget patch gcc-c++ java-11-openjdk java-11-openjdk-devel java-11-openjdk-headless gcc zip diffutils protobuf-c patch gcc-gfortran openssl-devel openssl python3.11-pip python3.11 python3.11-devel sudo
+yum install -y git make wget gcc-c++ java-11-openjdk java-11-openjdk-devel java-11-openjdk-headless gcc zip diffutils protobuf-c patch gcc-gfortran openssl-devel openssl python3.11-pip python3.11 python3.11-devel sudo
 
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk
 export PATH=$PATH:$JAVA_HOME/bin
 
 #install bazel
-wget https://github.com/bazelbuild/bazel/releases/download/6.0.0/bazel-6.0.0-dist.zip
-mkdir -p  bazel-6.0.0
-unzip bazel-6.0.0-dist.zip -d bazel-6.0.0/
-cd bazel-6.0.0/
+#install bazel
+wget https://github.com/bazelbuild/bazel/releases/download/6.4.0/bazel-6.4.0-dist.zip
+mkdir -p  bazel-6.4.0
+unzip bazel-6.4.0-dist.zip -d bazel-6.4.0/
+cd bazel-6.4.0/
 export EXTRA_BAZEL_ARGS="--host_javabase=@local_jdk//:jdk" bash
 ./compile.sh
 #export the path of bazel bin
@@ -44,18 +45,14 @@ git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-sudo curl https://sh.rustup.rs -sSf | sh -s -- -y
+curl https://sh.rustup.rs -sSf | sh -s -- -y
 PATH="$HOME/.cargo/bin:$PATH"
 source $HOME/.cargo/env
 rustc --version
 
-sudo pip3 install --upgrade setuptools
-sudo pip3 install --upgrade pip
-sudo pip3 install cryptography
-
 #Apply patch
-wget https://raw.githubusercontent.com/vinodk99/build-scripts/rules_python_fork/r/rules_python%20/rules_python_0.27.0.patch
-patch -p1 < rules_python_0.27.0.patch
+wget https://raw.githubusercontent.com/vinodk99/build-scripts/rules_python_fork/r/rules_python%20/rules_python_0.27.1.patch
+patch -p1 < rules_python_0.27.1.patch
 
 # Install
 if ! bazel build //... ; then
@@ -66,7 +63,7 @@ if ! bazel build //... ; then
 fi
 
 # Run the test cases
-if ! bazel test //... ; then
+if ! bazel test --flaky_test_attempts=3 --build_tests_only --local_test_jobs=12 --show_progress_rate_limit=5 --curses=yes --color=yes --terminal_columns=143 --show_timestamps --verbose_failures --jobs=30 --announce_rc --experimental_repository_cache_hardlinks --disk_cache= --sandbox_tmpfs_path=/tmp --experimental_build_event_json_file_path_conversion=false  --remote_max_connections=200 --test_tag_filters=-integration-test --test_env=HOME --test_env=BAZELISK_USER_AGENT ... ; then
     echo "------------------$PACKAGE_NAME:Build_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Build_success_but_test_Fails"
