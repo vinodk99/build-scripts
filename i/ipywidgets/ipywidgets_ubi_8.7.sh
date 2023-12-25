bdedfb76a311341b90846dfa0919ed1cfbfa1159
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2023-12-25 13:12:11 +0530

    Update ipywidgets_ubi_8.7.sh

--- i/ipywidgets/ipywidgets_ubi_8.7.sh
+++ i/ipywidgets/ipywidgets_ubi_8.7.sh
@@ -36,14 +36,15 @@ cd  $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 python3 -m pip install --upgrade pip
 
-if ! pip3 install file://$PWD/python/ipywidgets#egg=ipywidgets[test] ; then
+if ! pip3 install file://$PWD/python/ipywidgets#egg=ipywidgets[test] ;  then
     echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
+pip3 install pytest
 cd python/ipywidgets
-if ! pytest; then
+if ! pytest --cov=ipywidgets ipywidgets ; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
