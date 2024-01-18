2f5969de326a956a859b7d7884aeddc1286dbceb
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-01-18 16:32:54 +0530

    Update oreintdb_ubi_8.7.sh

--- o/orientdb/oreintdb_ubi_8.7.sh
+++ o/orientdb/oreintdb_ubi_8.7.sh
@@ -45,14 +45,7 @@ git clone $PACKAGE_URL
 cd $PACKAGE_NAME/
 git checkout $PACKAGE_VERSION
 
-if ! mvn clean install -DskipTests ; then
-    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
-    echo "$PACKAGE_URL $PACKAGE_NAME"
-    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
-    exit 1
-fi
-
-if ! mvn test ; then
+if ! mvn -B package --file pom.xml ; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
