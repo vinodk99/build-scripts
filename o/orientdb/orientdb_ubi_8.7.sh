bff53d33a8e7d5104c4594b482d163d765677314
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-02-16 14:47:35 +0530

    Update orientdb_ubi_8.7.sh

--- o/orientdb/orientdb_ubi_8.7.sh
+++ o/orientdb/orientdb_ubi_8.7.sh
@@ -45,7 +45,7 @@ git clone $PACKAGE_URL
 cd $PACKAGE_NAME/
 git checkout $PACKAGE_VERSION
 
-if ! mvn -B package --file pom.xml -Xmx1024m ; then
+if ! mvn -B package --file pom.xml ; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
