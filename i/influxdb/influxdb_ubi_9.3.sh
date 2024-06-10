58fd02f080aed6a956b5d000fa95d6f254eb69a6
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-06-10 07:17:06 +0000

    updated script

--- i/influxdb/influxdb_ubi_9.3.sh
+++ i/influxdb/influxdb_ubi_9.3.sh
@@ -1,33 +1,11 @@
-#!/bin/bash -e
-# -----------------------------------------------------------------------------
-#
-# Package	    : influxdb
-# Version	    : v2.7.6
-# Source repo	: https://github.com/influxdata/influxdb.git
-# Tested on	    : UBI:9.3
-# Language      : Go 
-# Travis-Check  : True
-# Script License: Apache License, Version 2 or later
-# Maintainer	: Vinod K <Vinod.K1@ibm.com>
-#
-# Disclaimer: This script has been tested in root mode on given
-# ==========  platform using the mentioned version of the package.
-#             It may not work as expected with newer versions of the
-#             package and/or distribution. In such case, please
-#             contact "Maintainer" of this script.
-#
-# ----------------------------------------------------------------------------
-
 PACKAGE_NAME=influxdb
 PACKAGE_VERSION=${1:-v2.7.6}
 PACKAGE_URL=https://github.com/influxdata/influxdb
 
-# Install dependencies
-yum install -y gcc gcc-c++ python3 python3-devel git wget sudo make autoconf automake \
-               zlib-devel bzip2 bzip2-devel xz-devel curl-devel openssl-devel \
-               ncurses-devel diffutils libtool json-c.ppc64le elfutils-libelf.ppc64le cmake patch libcap
+yum install -y gcc gcc-c++ python3 python3-devel git wget sudo make autoconf automake zlib-devel bzip2 bzip2-devel xz-devel curl-devel openssl-devel
+yum install -y ncurses-devel diffutils libtool json-c.ppc64le elfutils-libelf.ppc64le cmake patch libcap
 
-# Install Protocol Buffers
+#install protbuf
 git clone https://github.com/protocolbuffers/protobuf
 cd protobuf
 git checkout v3.17.3
@@ -37,10 +15,8 @@ make
 make install
 cd ..
 
-# Set PKG_CONFIG_PATH for protobuf
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
-
-# Install Protobuf-C
+#install protoc-c
 git clone https://github.com/protobuf-c/protobuf-c
 cd protobuf-c
 git checkout v1.4.0
@@ -50,48 +26,32 @@ make
 make install
 cd ..
 
-# Install Rust
+#install rustc
 curl https://sh.rustup.rs -sSf | sh -s -- -y
+PATH="$HOME/.cargo/bin:$PATH"
 source $HOME/.cargo/env
 rustc --version
 
-# Install Go
 GO_VERSION=1.21.6
 wget https://golang.org/dl/go$GO_VERSION.linux-ppc64le.tar.gz
 tar -C /usr/local -xzf go$GO_VERSION.linux-ppc64le.tar.gz
 rm -rf go$GO_VERSION.linux-ppc64le.tar.gz
 export GOROOT=/usr/local/go
-export GOPATH=$HOME/go
+export GOPATH=$HOME
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
-# Install Flux
-git clone https://github.com/influxdata/flux
-cd flux
-make
-make install
-cd ..
-
-# Find the directory containing `flux.pc`
-FLUX_PC_PATH=$(find /usr/local -name flux.pc | xargs dirname)
-
-# Update PKG_CONFIG_PATH for flux
-export PKG_CONFIG_PATH=$FLUX_PC_PATH:$PKG_CONFIG_PATH
-
-# Clone InfluxDB repository and checkout the specified version
 git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-# Build InfluxDB
-if ! go build ./...; then
+if ! make; then
     echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
 
-# Test InfluxDB build
-if ! go test -race ./...; then
+if ! make test; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
