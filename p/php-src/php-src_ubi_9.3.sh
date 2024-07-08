1989bcfb5acb3290717df3e80784eaa65736b838
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-07-08 07:03:43 +0000

    updated

--- p/php-src/php-src_ubi_9.3.sh
+++ p/php-src/php-src_ubi_9.3.sh
@@ -49,12 +49,8 @@ if ! make -j$(/usr/bin/nproc) && make install; then
     exit 1
 fi
 
-# Skip TTY test if not available
-sed -i '/stream_isatty/d' tests/output/stream_isatty_in-out.phpt
-
 # Run the tests
-if ! sapi/cli/php run-tests.php --asan -P -q -d opcache.jit=disable -d opcache.protect_memory=1 -d opcache.jit_buffer_size=64M -j4 -g FAIL,BORK,LEAK,XLEAK --no-progress --offline --show-diff --show-slow 1000 --set-timeout 120 ; then
-    echo "------------------$PACKAGE_NAME::Build_and_Test_fails-------------------------"
+if ! sapi/cli/php run-tests.php -P --asan -q -d zend_extension=opcache.so -d opcache.enable_cli=1 -d opcache.jit=tracing -d opcache.protect_memory=1 -d opcache.jit_buffer_size=64M -j2 -g FAIL,BORK,LEAK,XLEAK --no-progress --offline --show-diff --show-slow 1000 --set-timeout 120
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | $OS_NAME | GitHub  | Fail|  Build_and_Test_fails"
     exit 2
