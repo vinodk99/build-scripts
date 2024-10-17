a3a715b43f6c3d1c2030795778f0f0a54853fb10
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2024-10-17 11:29:58 +0000

    Currency:updated eclipse-cllections

--- e/eclipse-collections/eclipse-collections_ubi_9.3.sh
+++ e/eclipse-collections/eclipse-collections_ubi_9.3.sh
@@ -24,15 +24,15 @@ PACKAGE_URL=https://github.com/eclipse/eclipse-collections.git
 PACKAGE_VERSION=${1:-11.1.0}
 
 # install tools and dependent packages
-yum install -y java-11-openjdk java-11-openjdk-devel java-11-openjdk-headless git wget
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk
+yum install -y java-17-openjdk java-17-openjdk-devel java-17-openjdk-headless git wget
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
 export PATH=$JAVA_HOME/bin:$PATH
 
 # install maven
-wget https://archive.apache.org/dist/maven/maven-3/3.8.7/binaries/apache-maven-3.8.7-bin.tar.gz
-tar -zxf apache-maven-3.8.7-bin.tar.gz
-cp -R apache-maven-3.8.7 /usr/local
-ln -sf /usr/local/apache-maven-3.8.7/bin/mvn /usr/bin/mvn
+wget https://archive.apache.org/dist/maven/maven-3/3.9.7/binaries/apache-maven-3.9.7-bin.tar.gz
+tar -zxf apache-maven-3.9.7-bin.tar.gz
+cp -R apache-maven-3.9.7 /usr/local
+ln -sf /usr/local/apache-maven-3.9.7/bin/mvn /usr/bin/mvn
 
 # Cloning the repository
 git clone $PACKAGE_URL
@@ -40,7 +40,7 @@ cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
 #Build
-if ! mvn -B clean install -fae ; then
+if ! mvn -B clean install -fae -Dmaven=3.9.7 ; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
@@ -48,7 +48,7 @@ if ! mvn -B clean install -fae ; then
 fi
 
 #Test
-if ! mvn test -DforkCount=2 ; then
+if ! ./mvnw --color=always verify ; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
