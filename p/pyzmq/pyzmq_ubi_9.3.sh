9953432cbaa5cdc2a1c0f29a66124a2df7b4230e
vinodk99 <109129753+vinodk99@users.noreply.github.com>
2025-04-17 15:00:14 +0530

    Update pyzmq_ubi_9.3.sh

--- p/pyzmq/pyzmq_ubi_9.3.sh
+++ p/pyzmq/pyzmq_ubi_9.3.sh
@@ -2,7 +2,7 @@
 # -----------------------------------------------------------------------------
 #
 # Package          : pyzmq
-# Version          : v26.4.0
+# Version          : v26.3.0
 # Source repo      : https://github.com/zeromq/pyzmq.git
 # Tested on        : UBI:9.5
 # Language         : Python
@@ -27,7 +27,7 @@ CURRENT_DIR="${PWD}"
 
 # Install dependencies
 yum install -y git gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ gcc-toolset-13-gcc-gfortran \
-    make wget openssl-devel bzip2-devel glibc-static libstdc++-static libffi-devel \
+    cmake make wget openssl-devel bzip2-devel glibc-static libstdc++-static libffi-devel \
     zlib-devel python-devel python-pip pkg-config automake autoconf libtool
 
 source /opt/rh/gcc-toolset-13/enable
@@ -39,7 +39,7 @@ git clone $PACKAGE_URL
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-pip install cython==3.0.12 packaging==24.2 pathspec==0.12.1 scikit-build-core==0.11.1 cmake==3.27.9 ninja==1.11.1.4 build pytest
+pip install cython==3.0.12 packaging==24.2 pathspec==0.12.1 scikit-build-core==0.11.1 cmake==3.27.9 ninja==1.11.1.4 build pytest pytest-asyncio pytest-timeout
 
 #install
 if ! pip install -e . ; then
@@ -48,11 +48,8 @@ if ! pip install -e . ; then
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
-
-python3 -m build --wheel --no-isolation --outdir="$CURRENT_DIR"
-
 # Run tests
-if ! pytest -v  --capture=no -p no:warnings --ignore=tests/test_draft.py; then
+if ! pytest -v --timeout=60 --capture=no -p no:warnings --ignore=tests/test_draft.py --ignore=tests/test_cython.py ; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
@@ -61,5 +58,13 @@ else
     echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
-        exit 0
+fi
+
+# Building wheel with script itself as it needs to locate the libzmq target file 
+if ! python3 -m build --wheel --no-isolation --outdir="$CURRENT_DIR"; then
+    echo "------------------$PACKAGE_NAME: Wheel Build Failed ---------------------"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME: Wheel Build Success -------------------------"
+    exit 0
 fi
